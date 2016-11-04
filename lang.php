f852e5928913e03f066b06f237436dd7f3395f9c
a-dibrova <alekseydibrova1990@gmail.com>
2016-11-04 20:41:18 +0200

    Add translations for texts and fix issue with modify header warning

--- lang.php
+++ lang.php
@@ -1,74 +1,72 @@
-﻿<?php
+<?php
+$lang = array();
 
-$lang=array();
+$lang["settings_title"] = array();
+$lang["settings_title"]["en"] = __("Hurrakify Settings", 'hurrakify');
+$lang["settings_title"]["eo"] = __("Configuraciones de Hurrakify", 'hurrakify');
+$lang["settings_title"]["de"] = __("Hurrakify Einstellungen", 'hurrakify');
 
-$lang["settings_title"]=array();
-$lang["settings_title"]["en"]="Hurrakify Settings";
-$lang["settings_title"]["eo"]="Configuraciones de Hurrakify";
-$lang["settings_title"]["de"]="Hurrakify Einstellungen";
+$lang["settings_description"] = array();
+$lang["settings_description"]["en"] = __("Please update settings as per your requirement.", 'hurrakify');
+$lang["settings_description"]["eo"] = __("Por favor adapte las configuraciones a sus necesidades.", 'hurrakify');
+$lang["settings_description"]["de"] = __("Bitte passen Sie die Einstellungen ihren Wünschen an", 'hurrakify');
 
-$lang["settings_description"]=array();
-$lang["settings_description"]["en"]="Please update settings as per your requirement.";
-$lang["settings_description"]["eo"]="Por favor adapte las configuraciones a sus necesidades.";
-$lang["settings_description"]["de"]="Bitte passen Sie die Einstellungen ihren Wünschen an";
+$lang["wiki_links_title"] = array();
+$lang["wiki_links_title"]["en"] = __("Select a Easy-to-read Wiki", 'hurrakify');
+$lang["wiki_links_title"]["eo"] = __("Selecciona un Wiki en lenguaje llano", 'hurrakify');
+$lang["wiki_links_title"]["de"] = __("Wählen Sie ein Leichte Sprache Wiki aus", 'hurrakify');
 
-$lang["wiki_links_title"]=array();
-$lang["wiki_links_title"]["en"]="Select a Easy-to-read Wiki";
-$lang["wiki_links_title"]["eo"]="Selecciona un Wiki en lenguaje llano";
-$lang["wiki_links_title"]["de"]="Wählen Sie ein Leichte Sprache Wiki aus";
+$lang["wiki_links_desc"] = array();
+$lang["wiki_links_desc"]["en"] = __("Hurrakify will link to words in this Wiki.", 'hurrakify');
+$lang["wiki_links_desc"]["eo"] = __("Hurrakify enlazará palabras en este Wiki.", 'hurrakify');
+$lang["wiki_links_desc"]["de"] = __("Hurrakify wird zu Wörtern in diesem Wiki verlinken.", 'hurrakify');
 
-$lang["wiki_links_desc"]=array();
-$lang["wiki_links_desc"]["en"]="Hurrakify will link to words in this Wiki.";
-$lang["wiki_links_desc"]["eo"]="Hurrakify enlazará palabras en este Wiki.";
-$lang["wiki_links_desc"]["de"]="Hurrakify wird zu Wörtern in diesem Wiki verlinken.";
+$lang["wiki_links"] = array();
+$lang["wiki_links"]["en"] = array();
+$lang["wiki_links"]["en"]["title"] = __("English", 'hurrakify');
+$lang["wiki_links"]["en"]["master_url"] = "http://hurraki.org";
+$lang["wiki_links"]["en"]["url"] = "http://hurraki.org/english";
+$lang["wiki_links"]["en"]["api_url"] = "http://hurraki.org/english/w/api.php?action=parse&format=json&prop=text&section=0&noimages&disablepp&page=";
 
-$lang["wiki_links"]=array();
-$lang["wiki_links"]["en"]=array();
-$lang["wiki_links"]["en"]["title"]="English";
-$lang["wiki_links"]["en"]["master_url"]="http://hurraki.org";
-$lang["wiki_links"]["en"]["url"]="http://hurraki.org/english";
-$lang["wiki_links"]["en"]["api_url"]="http://hurraki.org/english/w/api.php?action=parse&format=json&prop=text&section=0&noimages&disablepp&page=";
+$lang["wiki_links"]["eo"] = array();
+$lang["wiki_links"]["eo"]["title"] = __("Espanol", 'hurrakify');
+$lang["wiki_links"]["eo"]["master_url"] = "http://hurraki.org";
+$lang["wiki_links"]["eo"]["url"] = "http://hurraki.org/espanol";
+$lang["wiki_links"]["eo"]["api_url"] = "http://hurraki.org/espanol/w/api.php?action=parse&format=json&prop=text&section=0&noimages&disablepp&page=";
 
-$lang["wiki_links"]["eo"]=array();
-$lang["wiki_links"]["eo"]["title"]="Espanol";
-$lang["wiki_links"]["eo"]["master_url"]="http://hurraki.org";
-$lang["wiki_links"]["eo"]["url"]="http://hurraki.org/espanol";
-$lang["wiki_links"]["eo"]["api_url"]="http://hurraki.org/espanol/w/api.php?action=parse&format=json&prop=text&section=0&noimages&disablepp&page=";
-
-$lang["wiki_links"]["de"]=array();
-$lang["wiki_links"]["de"]["title"]="Deutsch";
-$lang["wiki_links"]["de"]["url"]="http://hurraki.de";
-$lang["wiki_links"]["de"]["master_url"]="http://hurraki.de";
-$lang["wiki_links"]["de"]["api_url"]="http://hurraki.de/w/api.php?action=parse&format=json&prop=text&section=0&noimages&disablepp&page=";
+$lang["wiki_links"]["de"] = array();
+$lang["wiki_links"]["de"]["title"] = __("Deutsch", 'hurrakify');
+$lang["wiki_links"]["de"]["url"] = "http://hurraki.de";
+$lang["wiki_links"]["de"]["master_url"] = "http://hurraki.de";
+$lang["wiki_links"]["de"]["api_url"] = "http://hurraki.de/w/api.php?action=parse&format=json&prop=text&section=0&noimages&disablepp&page=";
 
 
-$lang["max_limit"]=array();
-$lang["max_limit"]["en"]="Maximum Tooltips";
-$lang["max_limit"]["eo"]="Máximo de tooltips";
-$lang["max_limit"]["de"]="Maximum Tooltips";
+$lang["max_limit"] = array();
+$lang["max_limit"]["en"] = __("Maximum Tooltips", 'hurrakify');
+$lang["max_limit"]["eo"] = __("Máximo de tooltips", 'hurrakify');
+$lang["max_limit"]["de"] = __("Maximum Tooltips", 'hurrakify');
 
-$lang["read_more"]=array();
-$lang["read_more"]["en"]="more";
-$lang["read_more"]["eo"]="continuar";
-$lang["read_more"]["de"]="mehr";
+$lang["read_more"] = array();
+$lang["read_more"]["en"] = __("more", 'hurrakify');
+$lang["read_more"]["eo"] = __("continuar", 'hurrakify');
+$lang["read_more"]["de"] = __("mehr", 'hurrakify');
 
-$lang["max_limit_descrip"]=array();
-$lang["max_limit_descrip"]["en"]="How many words should Hurrakify link at most? (Default setting is 10)";
-$lang["max_limit_descrip"]["eo"]="¿Cuántas palabras debe Hurrakify enlazar como máximo? (Configuración estándar es 10)";
-$lang["max_limit_descrip"]["de"]="Wie viele Wörter soll Hurrakify maximal verlinken. (Standardeinstellung ist 10)";
+$lang["max_limit_descrip"] = array();
+$lang["max_limit_descrip"]["en"] = __("How many words should Hurrakify link at most? (Default setting is 10)", 'hurrakify');
+$lang["max_limit_descrip"]["eo"] = __("¿Cuántas palabras debe Hurrakify enlazar como máximo? (Configuración estándar es 10)", 'hurrakify');
+$lang["max_limit_descrip"]["de"] = __("Wie viele Wörter soll Hurrakify maximal verlinken. (Standardeinstellung ist 10)", 'hurrakify');
 
-$lang["tooltip_on_title"]=array();
-$lang["tooltip_on_title"]["en"]="Choose an area";
-$lang["tooltip_on_title"]["eo"]="Selecciona el ámbito";
-$lang["tooltip_on_title"]["de"]="Wählen Sie den Bereich";
+$lang["tooltip_on_title"] = array();
+$lang["tooltip_on_title"]["en"] = __("Choose an area", 'hurrakify');
+$lang["tooltip_on_title"]["eo"] = __("Selecciona el ámbito", 'hurrakify');
+$lang["tooltip_on_title"]["de"] = __("Wählen Sie den Bereich", 'hurrakify');
 
-$lang["tooltip_on_desc"]=array();
-$lang["tooltip_on_desc"]["en"]="Choose the area (posts, pages etc.) in which Hurrakify should link difficult words.";
-$lang["tooltip_on_desc"]["eo"]="Selecciona el ámbito (contribuciones, páginas etc.) en que Hurrakify debe enlazar palabras difíciles.";
-$lang["tooltip_on_desc"]["de"]="Wählen Sie den Bereich (Beiträge, Seiten und so weiter) in dem Hurrakify schwere Wörter verlinken soll.";
+$lang["tooltip_on_desc"] = array();
+$lang["tooltip_on_desc"]["en"] = __("Choose the area (posts, pages etc.) in which Hurrakify should link difficult words.", 'hurrakify');
+$lang["tooltip_on_desc"]["eo"] = __("Selecciona el ámbito (contribuciones, páginas etc.) en que Hurrakify debe enlazar palabras difíciles.", 'hurrakify');
+$lang["tooltip_on_desc"]["de"] = __("Wählen Sie den Bereich (Beiträge, Seiten und so weiter) in dem Hurrakify schwere Wörter verlinken soll.", 'hurrakify');
 
-$lang["curl_error_mgs"]=array();
-$lang["curl_error_mgs"]["en"]="<p>Your server don't support <strong>cUrl</strong> which is must to use this plugin. Please activate  <strong>cUrl</strong> unless this plugin will not work</p>";
-$lang["curl_error_mgs"]["eo"]="<p>Your server don't support<strong>cUrl</strong> which is must to use this plugin. Please activate <strong>cUrl</strong> unless this plugin will not work</p>";
-$lang["curl_error_mgs"]["de"]="<p>Your server don't support<strong>cUrl</strong> which is must to use this plugin. Please activate <strong>cUrl</strong> unless this plugin will not work</p>";
-?>
+$lang["curl_error_mgs"] = array();
+$lang["curl_error_mgs"]["en"] = __("<p>Your server don't support <strong>cUrl</strong> which is must to use this plugin. Please activate  <strong>cUrl</strong> unless this plugin will not work</p>", 'hurrakify');
+$lang["curl_error_mgs"]["eo"] = __("<p>Your server don't support<strong>cUrl</strong> which is must to use this plugin. Please activate <strong>cUrl</strong> unless this plugin will not work</p>", 'hurrakify');
+$lang["curl_error_mgs"]["de"] = __("<p>Your server don't support<strong>cUrl</strong> which is must to use this plugin. Please activate <strong>cUrl</strong> unless this plugin will not work</p>", 'hurrakify');
