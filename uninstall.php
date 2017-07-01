e25e07041cb0a4749656d0bcee5c4cc99ef5aa49
topdeve <raymondanderson1988@gmail.com>
2017-07-01 13:32:52 -0700

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -6,5 +6,6 @@ if (WP_UNINSTALL_PLUGIN) {
     delete_option("hurraki_tooltip_key_words_en");
     delete_option("hurraki_tooltip_key_words_eo");
     delete_option("hurraki_tooltip_key_words_de");
+    delete_option("hurraki_tooltip_key_words_ma");
     delete_option("hurraki_tooltip_key_words_last_update_time");
 }
